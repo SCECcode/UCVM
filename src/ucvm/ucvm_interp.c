1f0c9fdc322a48f3e6119267b30c3534c01464b6
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2023-03-05 07:31:56 +0000

    local changes

--- src/ucvm/ucvm_interp.c
+++ src/ucvm/ucvm_interp.c
@@ -13,15 +13,9 @@ extern double ucvm_interp_vs_floor;
 extern double ucvm_interp_vp_floor;
 extern double ucvm_interp_density_floor;
 
-int JUST_TAPER=0;
-int taper1_cnt=0;
-int taper2_cnt=0;
-int taper3_cnt=0;
-int taper4_cnt=0;
-
 /* Ely interpolation method */
 int ucvm_interp_ely(double zmin, double zmax, ucvm_ctype_t cmode,
-		    ucvm_point_t *pnt, ucvm_data_t *data)
+            ucvm_point_t *pnt, ucvm_data_t *data)
 {
   double z, f, g;
 
@@ -62,7 +56,7 @@ int ucvm_interp_ely(double zmin, double zmax, ucvm_ctype_t cmode,
 
     /* Check that all crust properties and gtl vs are defined */
     if ((data->crust.vp <= 0.0) || (data->crust.vs <= 0.0) || 
-	(data->crust.rho <= 0.0) || (data->gtl.vs <= 0.0)) {
+    (data->crust.rho <= 0.0) || (data->gtl.vs <= 0.0)) {
       return(UCVM_CODE_NODATA);
     }
 
@@ -84,7 +78,7 @@ int ucvm_interp_ely(double zmin, double zmax, ucvm_ctype_t cmode,
 
 /* Taper interpolation method */
 int ucvm_interp_taper(double zmin, double zmax, ucvm_ctype_t cmode,
-		    ucvm_point_t *pnt, ucvm_data_t *data)
+            ucvm_point_t *pnt, ucvm_data_t *data)
 {
   double z, f, g;
 //  fprintf(stderr,"XXX callin interp for data depth %lf\n", data->depth);
@@ -126,7 +120,7 @@ int ucvm_interp_taper(double zmin, double zmax, ucvm_ctype_t cmode,
 
     /* Check that all crust properties and gtl vs are defined */
     if ((data->crust.vp <= 0.0) || (data->crust.vs <= 0.0) || 
-	(data->crust.rho <= 0.0) || (data->gtl.vs <= 0.0)) {
+    (data->crust.rho <= 0.0) || (data->gtl.vs <= 0.0)) {
       return(UCVM_CODE_NODATA);
     }
 
@@ -142,8 +136,6 @@ int ucvm_interp_taper(double zmin, double zmax, ucvm_ctype_t cmode,
     double taper_data_cmb_rho = ucvm_nafe_drake_rho(taper_data_cmb_vp);
 
 
-//fprintf(stderr,"XXX crust/taper %lf > %lf \n", data->interp_crust.vs , taper_data_cmb_vs);
-
     if(data->interp_crust.vs < taper_data_cmb_vs) {
       data->cmb.vs = data->interp_crust.vs;
       data->cmb.vp = data->interp_crust.vp;
@@ -154,49 +146,32 @@ int ucvm_interp_taper(double zmin, double zmax, ucvm_ctype_t cmode,
         data->cmb.rho = taper_data_cmb_rho;
     }
 
-    if(JUST_TAPER) {
-        data->cmb.vs = taper_data_cmb_vs;
-        data->cmb.vp = taper_data_cmb_vp;
-        data->cmb.rho = taper_data_cmb_rho;
-    }
-
   }
 
-  double ratio = data->cmb.vp / data->cmb.vs;
-  if(ucvm_interp_vs_floor != UCVM_DEFAULT_NULL_FLOOR && 
-		  data->cmb.vs < ucvm_interp_vs_floor) 
-    {  data->cmb.vs= ucvm_interp_vs_floor ; }
   if(ucvm_interp_vp_floor != UCVM_DEFAULT_NULL_FLOOR && 
-		  data->cmb.vp < ucvm_interp_vp_floor)
+          data->cmb.vp < ucvm_interp_vp_floor)
     { data->cmb.vp= ucvm_interp_vp_floor ; }
+
+  if(ucvm_interp_vs_floor != UCVM_DEFAULT_NULL_FLOOR && 
+          data->cmb.vs < ucvm_interp_vs_floor) 
+    { data->cmb.vs= ucvm_interp_vs_floor ; }
+
   if(ucvm_interp_density_floor != UCVM_DEFAULT_NULL_FLOOR && 
-		  data->cmb.rho < ucvm_interp_density_floor) 
+          data->cmb.rho < ucvm_interp_density_floor) 
     { data->cmb.rho= ucvm_interp_density_floor ; }
 
-  if( ratio < 1.45 )
-    { taper1_cnt=taper1_cnt+1; } 
-
   if( (data->cmb.vp / data->cmb.vs) < 1.45)
-    { data->cmb.vs= data->cmb.vp/1.45;
-      taper2_cnt=taper2_cnt+1; 
-    }
+    { data->cmb.vs= data->cmb.vp/1.45; }
 
-  if( data->cmb.vs < 500.0)
+  if(data->cmb.vs < 500.0)
     {
-      ratio = data->cmb.vp / data->cmb.vs;
-      taper3_cnt=taper3_cnt+1; 
-      data->cmb.vs=500;
-      data->cmb.vp= data->cmb.vs * ( ratio );
-    }
-  if(ucvm_interp_vp_floor != UCVM_DEFAULT_NULL_FLOOR && 
-		  data->cmb.vp < ucvm_interp_vp_floor)
-    { 
-       taper4_cnt=taper4_cnt+1;
-       data->cmb.vp= ucvm_interp_vp_floor ; 
-    }
+       double ratio = (data->cmb.vp/data->cmb.vs);
+       data->cmb.vs=500.0;
+       data->cmb.vp= data->cmb.vs * (ratio);
+    }        
 
-fprintf(stderr,"XXX 1(%d) 2(%d) 3(%d) 4(%d) \n", taper1_cnt, taper2_cnt, taper3_cnt, taper4_cnt);
-
+  if(data->cmb.vp < ucvm_interp_vp_floor) 
+    { data->cmb.vp= ucvm_interp_vp_floor ; }
 
   return(UCVM_CODE_SUCCESS);
 }
@@ -204,7 +179,7 @@ fprintf(stderr,"XXX 1(%d) 2(%d) 3(%d) 4(%d) \n", taper1_cnt, taper2_cnt, taper3_
 
 /* Linear interpolation method */
 int ucvm_interp_linear(double zmin, double zmax, ucvm_ctype_t cmode,
-		       ucvm_point_t *pnt, ucvm_data_t *data)
+               ucvm_point_t *pnt, ucvm_data_t *data)
 {
   double zratio;
 
@@ -233,8 +208,8 @@ int ucvm_interp_linear(double zmin, double zmax, ucvm_ctype_t cmode,
 
     /** MEI
     if ((data->crust.vp <= 0.0) || (data->crust.vs <= 0.0) || 
-	(data->crust.rho <= 0.0) || (data->gtl.vp <= 0.0) || 
-	(data->gtl.vs <= 0.0) || (data->gtl.rho <= 0.0)) {
+    (data->crust.rho <= 0.0) || (data->gtl.vp <= 0.0) || 
+    (data->gtl.vs <= 0.0) || (data->gtl.rho <= 0.0)) {
       return(UCVM_CODE_NODATA);
     }
     **/
@@ -242,15 +217,15 @@ int ucvm_interp_linear(double zmin, double zmax, ucvm_ctype_t cmode,
     zratio = (data->depth - zmin) / (zmax - zmin);
     if(data->gtl.vp > 0.0 && data->crust.vp > 0.0) {
       data->cmb.vp = interpolate_linear(data->gtl.vp, 
-				      data->crust.vp, zratio);
+                      data->crust.vp, zratio);
     }
     if(data->gtl.vs > 0.0 && data->crust.vs > 0.0) {
       data->cmb.vs = interpolate_linear(data->gtl.vs, 
-				      data->crust.vs, zratio);
+                      data->crust.vs, zratio);
     }
     if(data->gtl.rho > 0.0 && data->crust.rho > 0.0) {
       data->cmb.rho = interpolate_linear(data->gtl.rho, 
-				       data->crust.rho, zratio);
+                       data->crust.rho, zratio);
     } 
   } else {
     data->cmb.vp = data->crust.vp;
@@ -265,7 +240,7 @@ int ucvm_interp_linear(double zmin, double zmax, ucvm_ctype_t cmode,
 
 /* Crustal pass-through method */
 int ucvm_interp_crustal(double zmin, double zmax, ucvm_ctype_t cmode,
-			ucvm_point_t *pnt, ucvm_data_t *data)
+            ucvm_point_t *pnt, ucvm_data_t *data)
 {
   data->cmb.vp = data->crust.vp;
   data->cmb.vs = data->crust.vs;
