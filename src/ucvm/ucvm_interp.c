bc98b92cf2d7aeb890a2b2e1db6ab41f7509d8b3
Mei-Hui Su <mei@discovery2.hpc.usc.edu>
2023-03-04 22:25:17 -0800

    use scott's updated constraints
    Vs >= 500 m/s. If lower, Calculate the Vp/Vs ratio. Set Vs=500, and Vp=Vs*(Vp/Vs ratio).
    Vp >= 1700 m/s. If lower, Vp is set to 1700.
    rho = 1700 km/m3. If lower, rho is set to 1700.
    Vp/Vs >= 1.45. If not, Vs is set to Vp/1.45.

--- src/ucvm/ucvm_interp.c
+++ src/ucvm/ucvm_interp.c
@@ -13,12 +13,6 @@ extern double ucvm_interp_vs_floor;
 extern double ucvm_interp_vp_floor;
 extern double ucvm_interp_density_floor;
 
-int JUST_TAPER=0;
-int taper1_cnt=0;
-int taper2_cnt=0;
-int taper3_cnt=0;
-int taper4_cnt=0;
-
 /* Ely interpolation method */
 int ucvm_interp_ely(double zmin, double zmax, ucvm_ctype_t cmode,
 		    ucvm_point_t *pnt, ucvm_data_t *data)
@@ -154,49 +148,26 @@ int ucvm_interp_taper(double zmin, double zmax, ucvm_ctype_t cmode,
         data->cmb.rho = taper_data_cmb_rho;
     }
 
-    if(JUST_TAPER) {
-        data->cmb.vs = taper_data_cmb_vs;
-        data->cmb.vp = taper_data_cmb_vp;
-        data->cmb.rho = taper_data_cmb_rho;
-    }
-
   }
 
-  double ratio = data->cmb.vp / data->cmb.vs;
-  if(ucvm_interp_vs_floor != UCVM_DEFAULT_NULL_FLOOR && 
-		  data->cmb.vs < ucvm_interp_vs_floor) 
-    {  data->cmb.vs= ucvm_interp_vs_floor ; }
-  if(ucvm_interp_vp_floor != UCVM_DEFAULT_NULL_FLOOR && 
-		  data->cmb.vp < ucvm_interp_vp_floor)
+  if(ucvm_interp_vs_floor != UCVM_DEFAULT_NULL_FLOOR &&
+          data->cmb.vs < ucvm_interp_vs_floor)
+    { 
+       double ratio = (data->cmb.vp/data->cmb.vs);
+       data->cmb.vs= ucvm_interp_vs_floor ;
+       data->cmb.vp= data->cmb.vs * (ratio);
+    }
+
+  if(ucvm_interp_vp_floor != UCVM_DEFAULT_NULL_FLOOR &&
+          data->cmb.vp < ucvm_interp_vp_floor)
     { data->cmb.vp= ucvm_interp_vp_floor ; }
-  if(ucvm_interp_density_floor != UCVM_DEFAULT_NULL_FLOOR && 
-		  data->cmb.rho < ucvm_interp_density_floor) 
+
+  if(ucvm_interp_density_floor != UCVM_DEFAULT_NULL_FLOOR &&
+          data->cmb.rho < ucvm_interp_density_floor)
     { data->cmb.rho= ucvm_interp_density_floor ; }
 
-  if( ratio < 1.45 )
-    { taper1_cnt=taper1_cnt+1; } 
-
   if( (data->cmb.vp / data->cmb.vs) < 1.45)
-    { data->cmb.vs= data->cmb.vp/1.45;
-      taper2_cnt=taper2_cnt+1; 
-    }
-
-  if( data->cmb.vs < 500.0)
-    {
-      ratio = data->cmb.vp / data->cmb.vs;
-      taper3_cnt=taper3_cnt+1; 
-      data->cmb.vs=500;
-      data->cmb.vp= data->cmb.vs * ( ratio );
-    }
-  if(ucvm_interp_vp_floor != UCVM_DEFAULT_NULL_FLOOR && 
-		  data->cmb.vp < ucvm_interp_vp_floor)
-    { 
-       taper4_cnt=taper4_cnt+1;
-       data->cmb.vp= ucvm_interp_vp_floor ; 
-    }
-
-fprintf(stderr,"XXX 1(%d) 2(%d) 3(%d) 4(%d) \n", taper1_cnt, taper2_cnt, taper3_cnt, taper4_cnt);
-
+    { data->cmb.vs= data->cmb.vp/1.45; }
 
   return(UCVM_CODE_SUCCESS);
 }
